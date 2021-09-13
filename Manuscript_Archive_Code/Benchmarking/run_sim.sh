e7593ad49911e8925f07ab2cd1de4d02c5364246
Bokai Zhu <36682386+BokaiZhu@users.noreply.github.com>
2021-09-13 14:45:00 -0700

    Delete run_sim.sh

--- Manuscript_Archive_Code/Benchmarking/run_sim.sh
+++
@@ -1,7 +0,0 @@
-nohup bash run_delete_features.sh &> log/run_delete_features.log &
-nohup bash run_add_noise.sh &> log/run_add_noise.log &
-nohup bash run_vary_n_comp_ovlp.sh &> log/run_vary_n_comp_ovlp.log &
-nohup bash run_vary_n_comp_all.sh &> log/run_vary_n_comp_all.log &
-nohup bash run_vary_n_comp_ftr.sh &> log/run_vary_n_comp_ftr.log &
-nohup bash run_vary_n_clusters.sh &> log/run_vary_n_clusters.log &
-nohup bash run_vary_bad_prop.sh &> log/run_vary_bad_prop.log &
