d1520c699c8cde90c5b8ed1c06f90c07e81e968b
Bokai Zhu <36682386+BokaiZhu@users.noreply.github.com>
2021-10-20 22:00:05 -0700

    Update featuredelete_murine_slf.R

--- Manuscript_Archive_Code/Benchmarking/FigS4_2/featuredelete_murine_slf.R
+++ Manuscript_Archive_Code/Benchmarking/FigS4_2/featuredelete_murine_slf.R
@@ -8,7 +8,20 @@ path = "../Benchmarking/data/feature_deletion/murine/"
 x = read.csv(paste0(path0,"orig_x.csv"))
 y = read.csv(paste0(path0,"orig_y.csv"))
 ## some parameters
-feature_2_delete = c("CD1632","F480","Ly6G","Ly6C","CD2135","CD11b","CD11c","CD5","CD19","IgM","IgD","CD27","TCR","NKp46","B220","CD3","CD4")
+feature_2_delete = c("B220",
+        "CD106",
+        "CD11b",
+        "CD11c",
+        "CD1632",
+        "CD169",
+        "CD19",
+        "CD2135",
+        "CD27",
+        "CD3",
+        "CD31",
+        "CD4",
+        "CD44",
+        "CD45",)
 path_out = paste0(path,"seurat/")
 feature_2_delete = c("Full",feature_2_delete)
 ## container
@@ -104,10 +117,31 @@ write.csv(container,"../Benchmarking/data/feature_deletion/murine/seurat/metrics
 ##### liger start
 library(rliger)
 ## read in 
+## read in 
+path0 = "data/murine/mario/"
+path = "data/murine/"
+##### liger start
+library(rliger)
+## read in 
 x = read.csv(paste0(path0,"orig_x.csv"))
 y = read.csv(paste0(path0,"orig_y.csv"))
+#ll = c()
 ## some parameters
-feature_2_delete = c("CD1632","F480","Ly6G","Ly6C","CD2135","CD11b","CD11c","CD5","CD19","IgM","IgD","CD27","TCR","NKp46","B220","CD3","CD4")
+feature_2_delete = c("B220",
+                     "CD106",
+                     "CD11b",
+                     "CD11c",
+                     "CD1632",
+                     "CD169",
+                     "CD19",
+                     "CD2135",
+                     "CD27",
+                     "CD3",
+                     "CD31",
+                     "CD4",
+                     "CD44",
+                     "CD45",
+                     "CD5")
 path_out = paste0(path,"liger/")
 feature_2_delete = c("Full",feature_2_delete)
 ## container
@@ -115,12 +149,21 @@ coluse = c("n_deleted","1v1_acc","transfer_acc","prop_remain")
 container = matrix(NA, nrow = length(feature_2_delete), ncol = length(coluse))
 container = as.data.frame(container)
 colnames(container) = coluse
+
+## specific for rliger label
+x_temp = x
+y_temp = y
+rownames(x_temp)=paste0(as.character(rownames(x_temp)),"_x")
+rownames(y_temp)=paste0(as.character(rownames(y_temp)),"_y")
+##
+
 ## label
 x_label=x[,"label"]
 y_label=y[,"label"]
 x [,"label"] <- NULL
 y [,"label"] <- NULL
 ##
+
 idx = 0
 for (item in feature_2_delete){
   #print(item)
@@ -141,15 +184,15 @@ for (item in feature_2_delete){
   y_value_scale=as.data.frame(y_value_scale)
   rownames(y_value_scale)=paste0(as.character(rownames(y_value_scale)),"_y")
   # create seurat object datax
-  ligerobj=createLiger( list(x = t(x_value_scale), y = t(y_value_scale) ) )
+  ligerobj=createLiger( list(x = t(x_value_scale), y = t(y_value_scale) ), remove.missing = FALSE)
   ###Start integration, feature will drop so change this
   features=intersect(colnames(x),colnames(y))
   # default preprocessing
-  ligerobj <- rliger::normalize(ligerobj)
+  ligerobj <- rliger::normalize(ligerobj, remove.missing = FALSE)
   #ifnb_liger <- selectGenes(ifnb_liger, var.thresh = 0, alpha.thresh=1)
   ligerobj@var.genes=features #  just use all
-  ligerobj <- scaleNotCenter(ligerobj)
-  ligerobj <- optimizeALS(ligerobj, k = (length(features)-1))
+  ligerobj <- scaleNotCenter(ligerobj, remove.missing = FALSE)
+  ligerobj <- optimizeALS(ligerobj, k = (length(features)-1), remove.missing = FALSE)
   ligerobj <- quantile_norm(ligerobj)
   # remain porp
   container[idx,"prop_remain"] = NA
@@ -159,13 +202,28 @@ for (item in feature_2_delete){
   container[idx,"transfer_acc"] = NA
   # embedding
   embedding = ligerobj@H.norm # pca scores
-  container[idx,"n_deleted"] = idx -1 # just a useless column
+  
+  #### some special treatments for liger where cells could be deleted, need to get the correct original
+  rowname_remainx = rownames(ligerobj@scale.data$x)
+  rowname_remainy = rownames(ligerobj@scale.data$y)
+  orig_x = x_temp[rowname_remainx,]
+  orig_y = y_temp[rowname_remainy,] # this the original with possible deleted cells to avoid error for metrics
+  ####
+  
+  #container[idx,"n_deleted"] = idx -1 # just a useless column
   ###### end of the matching and integration process for current feature drop
   name_1 = paste0("embedding_x",idx-1,".csv")
   name_2 = paste0("embedding_y",idx-1,".csv")
-  write.csv(embedding[c(1:5000),], paste0(path_out,name_1), row.names=FALSE) # need to decide output pca cell
-  write.csv(embedding[c(5001:24980),], paste0(path_out,name_2), row.names=FALSE) # need to decide output pca cell
-  # some cell missing after reduction, possibily due to bad integration
+  
+  name_1_orig = paste0("orig_x",idx-1,".csv")
+  name_2_orig = paste0("orig_y",idx-1,".csv")
+  
+  write.csv(embedding[c(1:dim(orig_x)[1]),], paste0(path_out,name_1), row.names=FALSE) # need to decide output pca cell
+  write.csv(embedding[c((dim(orig_x)[1] + 1): dim(embedding)[1]),], paste0(path_out,name_2), row.names=FALSE) # need to decide output pca cell
+  ## for liger only the new original cells
+  write.csv(as.matrix(orig_x), paste0(path_out,name_1_orig), row.names=FALSE) # need to decide output pca cell
+  write.csv(as.matrix(orig_y), paste0(path_out,name_2_orig), row.names=FALSE) # need to decide output pca cell
+  
 }
 write.csv(container,"../Benchmarking/data/feature_deletion/murine/liger/metrics.csv")
 ###### fstmnn
@@ -175,7 +233,20 @@ library(batchelor)
 x = read.csv(paste0(path0,"orig_x.csv"))
 y = read.csv(paste0(path0,"orig_y.csv"))
 ## some parameters
-feature_2_delete = c("CD1632","F480","Ly6G","Ly6C","CD2135","CD11b","CD11c","CD5","CD19","IgM","IgD","CD27","TCR","NKp46","B220","CD3","CD4")
+feature_2_delete = c("B220",
+        "CD106",
+        "CD11b",
+        "CD11c",
+        "CD1632",
+        "CD169",
+        "CD19",
+        "CD2135",
+        "CD27",
+        "CD3",
+        "CD31",
+        "CD4",
+        "CD44",
+        "CD45")
 path_out = paste0(path,"fstmnn/")
 feature_2_delete = c("Full",feature_2_delete)
 ## container
